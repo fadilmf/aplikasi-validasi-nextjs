fc8bb570a7dadc9a782ae738fe4ba3669afb3b25
Rayyan Muhammad <71443030+raayyann@users.noreply.github.com>
2023-10-11 12:26:35 +0700

    add top loader (loading)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import BottomNavbar from "@/components/BottomNavbar";
 import AuthProvider from "@/components/AuthProviders";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
+          <NextTopLoader />
           <Navbar />
           <main className="flex-grow mt-14">
             <AuthProvider>{children}</AuthProvider>
